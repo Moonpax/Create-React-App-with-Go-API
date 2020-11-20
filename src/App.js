213f5379dc1d909716e6e890f254af1f722db718
Vadim Mityagin <vadim@beprotravel.com>
2020-11-20 16:24:16 +0200

    test go handler

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import { useEffect, useState } from 'react';
-import './App.css';
+import React from 'react'
+import { useEffect, useState } from 'react'
+import './App.css'
 
 function App() {
-  const [date, setDate] = useState(null);
+  const [date, setDate] = useState(null)
+  const [test, setTest] = useState(null)
   useEffect(() => {
     async function getDate() {
-      const res = await fetch('/api/date');
-      const newDate = await res.text();
-      setDate(newDate);
+      const res = await fetch('/api/date')
+      const newDate = await res.text()
+      setDate(newDate)
     }
-    getDate();
-  }, []);
+    getDate()
+
+    async function getTest() {
+      const res = await fetch('/api/test')
+      const newTest = await res.text()
+      setTest(newTest)
+    }
+    getTest()
+  }, [])
   return (
     <main>
       <h1>Create React App + Go API</h1>
@@ -49,9 +57,10 @@ function App() {
       </p>
       <br />
       <h2>The date according to Go is:</h2>
+      <p>{test ? test : 'Loading test...'}</p>
       <p>{date ? date : 'Loading date...'}</p>
     </main>
-  );
+  )
 }
 
-export default App;
+export default App
